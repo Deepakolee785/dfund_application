39106d1740260e23681812e4cd27e3c3f0634904
Deepak Oli <deepakolee785@gmail.com>
2021-02-09 15:09:01 +0545

    admin model updated

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose')
 
 const AdminSchema = new mongoose.Schema(
 	{
-		username: {
+		name: {
 			type: String,
 			required: true,
 			min: 5,
 		},
-
 		email: {
 			type: String,
 			required: true,
