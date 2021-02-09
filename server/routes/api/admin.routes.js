0df479cfb66b4edb699a0e494ab6e41aaea8005b
Deepak Oli <deepakolee785@gmail.com>
2021-02-09 15:37:43 +0545

    validation implemented

--- server/routes/api/admin.routes.js
+++ server/routes/api/admin.routes.js
@@ -2,28 +2,37 @@ const router = require('express').Router()
 const Admin = require('../../models/Admin')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const {
+	adminLoginValidation,
+	adminRegisterValidation,
+} = require('../../validation/auth.validation')
 
 router.post('/register', async (req, res) => {
+	// Validate
+	const { error } = adminRegisterValidation(req.body)
+	if (error)
+		return res.status(400).send({ message: error.details[0].message })
 	// Check if user already exists
 	const adminExist = await Admin.findOne({ email: req.body.email })
 	if (adminExist)
 		return res.status(400).send({
 			message: 'Email is already taken.',
 		})
-
 	// Hash the password
 	const salt = await bcrypt.genSalt(10)
 	const hashPassword = await bcrypt.hash(req.body.password, salt)
-	// create new user
+
+	// create new admin
 	const admin = new Admin({
-		name: req.body.Name,
+		name: req.body.name,
 		email: req.body.email,
 		password: hashPassword,
 	})
+
 	try {
 		const savedAdmin = await admin.save()
 		res.send({
-			admin: savedAdmin._id,
+			admin: { id: savedAdmin._id, name: savedAdmin.name },
 			message: 'Successfully registered admin',
 		})
 	} catch (error) {
@@ -32,10 +41,22 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
+	//validate
+	const { error } = adminLoginValidation(req.body)
+	if (error)
+		return res.status(400).send({
+			message: error.details[0].message,
+		})
 	// Check if email already exists
 	const admin = await Admin.findOne({
 		email: req.body.email,
 	})
+
+	// console.log('checked' + user)
+	if (!admin)
+		return res
+			.status(400)
+			.send({ message: 'Email or password is incorrect.' })
 
 	// Check if password is correct
 	const validPassword = await bcrypt.compare(
