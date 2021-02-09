8745dbd4add2e5f77516a697486a8958275f22a6
Deepak Oli <deepakolee785@gmail.com>
2021-02-09 14:36:31 +0545

    Campaign Model Schema created

---
+++ server/models/Campaign.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+
+const CampaignSchema = new mongoose.Schema(
+	{
+		addr: {
+			type: String,
+			required: true,
+		},
+
+		blockHash: {
+			type: String,
+			required: true,
+		},
+		blockNumber: {
+			type: Number,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+		},
+		country: {
+			type: String,
+			required: true,
+		},
+		creator: {
+			type: String,
+			required: true,
+		},
+		cumulativeGasUsed: {
+			type: String,
+			required: true,
+		},
+		deadline: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		goalAmount: {
+			type: String,
+			required: true,
+		},
+		imageHash: {
+			type: String,
+			required: true,
+		},
+		minimumContribution: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: Boolean,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		to: {
+			type: String,
+			required: true,
+		},
+		transactionHash: {
+			type: String,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Campaign', CampaignSchema)
