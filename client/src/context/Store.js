6c50d3510d03b3e894e69d45eb313a0aa0ba4f46
Deepak Oli <deepakolee785@gmail.com>
2021-02-11 17:46:28 +0545

    admin auth provided to global store

--- client/src/context/Store.js
+++ client/src/context/Store.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import FactoryState from './factory/FactoryState'
 import AuthState from './auth/AuthState'
+import AdminAuthState from './admin_auth/AdminAuthState'
 
 const StoreProvider = ({ children }) => {
 	return (
 		<FactoryState>
-			<AuthState>{children}</AuthState>
+			<AuthState>
+				<AdminAuthState>{children}</AdminAuthState>
+			</AuthState>
 		</FactoryState>
 	)
 }
