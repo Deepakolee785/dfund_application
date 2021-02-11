fa025c501896a44cf2f6bc5fb5a7b2eddd8ac56a
Deepak Oli <deepakolee785@gmail.com>
2021-02-11 17:47:37 +0545

    state removed from admin index

--- client/src/pages/admin/index.jsx
+++ client/src/pages/admin/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import AdminAuthSate from '../../context/admin_auth/AdminAuthState'
 import AdminLogin from './login/index.js'
 const AdminPage = () => {
 	return (
-		<AdminAuthSate>
+		<>
 			<AdminLogin />
-		</AdminAuthSate>
+		</>
 	)
 }
 
