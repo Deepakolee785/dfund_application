e4a0ce777a4b957a10f8b6a00fad252a63e346bb
Deepak Oli <deepakolee785@gmail.com>
2021-02-11 17:47:49 +0545

    login function added

--- client/src/pages/admin/login/index.js
+++ client/src/pages/admin/login/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useContext } from 'react'
 
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, message } from 'antd'
+import { useHistory } from 'react-router-dom'
 
 import AdminAuthContext from '../../../context/admin_auth/adminAuthContext'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min'
 const layout = {
 	labelCol: {
 		span: 8,
@@ -18,7 +20,15 @@ const tailLayout = {
 	},
 }
 const AdminLogin = () => {
-	const { loadUser } = useContext(AdminAuthContext)
+	const history = useHistory()
+	const {
+		loadUser,
+		login,
+		loading,
+		success,
+		error,
+		isAuthenticated,
+	} = useContext(AdminAuthContext)
 
 	useEffect(() => {
 		loadUser()
@@ -26,11 +36,26 @@ const AdminLogin = () => {
 	}, [])
 	const onFinish = values => {
 		console.log('Success:', values)
+		login(values)
 	}
 
 	const onFinishFailed = errorInfo => {
 		console.log('Failed:', errorInfo)
 	}
+
+	useEffect(() => {
+		if (error) {
+			message.error(error)
+		}
+	}, [error])
+	useEffect(() => {
+		if (success) {
+			message.success(success)
+			history.push('/control/home')
+		}
+		//eslint-disable-next-line
+	}, [success])
+	if (isAuthenticated) return <Redirect to='/control/home' />
 	return (
 		<div className='Center' style={{ height: '60vh' }}>
 			<h3>Admin Login</h3>
@@ -67,7 +92,7 @@ const AdminLogin = () => {
 				</Form.Item>
 
 				<Form.Item {...tailLayout}>
-					<Button type='primary' htmlType='submit'>
+					<Button type='primary' htmlType='submit' loading={loading}>
 						Submit
 					</Button>
 				</Form.Item>
