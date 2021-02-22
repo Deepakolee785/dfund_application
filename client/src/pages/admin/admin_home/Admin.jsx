e6aff7cec7264ec2c5050e8178c91b3954ae05d6
Deepak Oli <deepakolee785@gmail.com>
2021-02-22 19:13:45 +0545

    admin users displayed in table

--- client/src/pages/admin/admin_home/Admin.jsx
+++ client/src/pages/admin/admin_home/Admin.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+import { DEV_URL } from '../../../config'
+import { Table } from 'antd'
 const Admin = () => {
-	return <div>Admin</div>
+	const [admins, setAdmins] = useState([])
+	const [loading, setLoading] = useState(false)
+	useEffect(() => {
+		setLoading(true)
+		axios
+			.get(`${DEV_URL}/api/admin/get-all-admins`)
+			.then(res => {
+				// console.log(res.data)
+				setAdmins(res.data.admins)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+			.finally(() => setLoading(false))
+	}, [])
+
+	// console.log(admins)
+
+	const columns = [
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+		},
+
+		{
+			title: 'Email',
+			dataIndex: 'email',
+			key: 'email',
+		},
+	]
+
+	const dataSource = admins.map(admin => {
+		const { _id: id, name, email } = admin
+
+		return {
+			key: id,
+			email,
+			name,
+		}
+	})
+
+	return (
+		<div>
+			<h3>Admins</h3>
+			<Table
+				columns={columns}
+				dataSource={dataSource}
+				scroll={{ x: true }}
+				loading={loading}
+			/>
+		</div>
+	)
 }
 
 export default Admin
