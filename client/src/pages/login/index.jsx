367a2372ce4cccf21aa29d6865bd2007d3161ed1
Deepak Oli <deepakolee785@gmail.com>
2021-02-28 17:41:50 +0545

    input component used in login page

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Input, Button, message } from 'antd'
+import { Form, Input, message } from 'antd'
 import FactoryContext from '../../context/factory/factoryContext.js'
 import AuthContext from '../../context/auth/authContext'
 import history from '../../utils/history'
+
+import { Button } from '../../components/button'
 
 import wallet_icon from '../../assets/icons/wallet.svg'
 import user_icon from '../../assets/icons/user.svg'
 import lock_icon from '../../assets/icons/lock.svg'
 import Layout from '../../layout/user_layout'
+import InputEl from '../../components/input/index.jsx'
 
 const LoginPage = () => {
 	const [form] = Form.useForm()
@@ -60,19 +63,53 @@ const LoginPage = () => {
 						// {...layout}
 						name='basic'
 						form={form}
-						initialValues={{}}
 						onFinish={onFinish}
 						onFinishFailed={onFinishFailed}
 					>
-						<div>
+						<InputEl
+							label='Your Ethereum walllet'
+							name='wallet'
+							validationRule={[
+								{
+									required: true,
+									message: 'Wallet is required!',
+								},
+							]}
+							placeholder=''
+							icon={wallet_icon}
+							disabled={true}
+						/>
+						<InputEl
+							label='Username'
+							name='username'
+							validationRule={[
+								{
+									required: true,
+									message: 'Please input your username!',
+								},
+							]}
+							placeholder='Username'
+							icon={user_icon}
+							disabled={false}
+						/>
+						<InputEl
+							label='Password'
+							name='password'
+							validationRule={[
+								{
+									required: true,
+									message: 'Please input your password!',
+								},
+							]}
+							placeholder='Username'
+							icon={lock_icon}
+							disabled={false}
+							isPasswordField={true}
+						/>
+						{/* <div>
+
 							<h4>Your Ethereum walllet</h4>
-							<div
-								style={
-									{
-										// borderLeft: '7px solid #5F66F1',
-									}
-								}
-							>
+							<div>
 								<Form.Item
 									// label='Your Wallet'
 									name='wallet'
@@ -104,8 +141,9 @@ const LoginPage = () => {
 									/>
 								</Form.Item>
 							</div>
-						</div>
-						<div>
+						</div> */}
+
+						{/* <div>
 							<h4>Username</h4>
 							<div
 								style={
@@ -148,14 +186,8 @@ const LoginPage = () => {
 							</div>
 						</div>
 						<div>
-							<h4>Username</h4>
-							<div
-								style={
-									{
-										// borderLeft: '7px solid #5F66F1',
-									}
-								}
-							>
+							<h4>Password</h4>
+							<div>
 								<Form.Item
 									// label='Password'
 									name='password'
@@ -188,19 +220,25 @@ const LoginPage = () => {
 									/>
 								</Form.Item>
 							</div>
-						</div>
+						</div> */}
 						<Form.Item>
 							<Button
 								type='primary'
+								variant='primary'
 								htmlType='submit'
+								block
 								loading={loading}
 							>
 								Login
 							</Button>
 						</Form.Item>
 						<p>
-							Didn't have an Dfund account?
-							<Link to='/register'>Register</Link>
+							Didn't have an Dfund account?{' '}
+							<Link to='/register'>
+								<strong style={{ textDecoration: 'underline' }}>
+									Register
+								</strong>
+							</Link>
 						</p>
 					</Form>
 				</div>
