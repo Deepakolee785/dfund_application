ce560bd6374ad6a826ebd9e5a592f3234552b470
Deepak Oli <deepakolee785@gmail.com>
2021-02-28 19:12:42 +0545

    register page ui

--- client/src/pages/register/index.jsx
+++ client/src/pages/register/index.jsx
@@ -1,9 +1,23 @@
-import React, { useEffect, useContext } from 'react'
-import { Form, Input, Button, message } from 'antd'
+import React, { useEffect, useContext, useState } from 'react'
+import { Form, message, Input, Spin } from 'antd'
 import FactoryContext from '../../context/factory/factoryContext'
 import AuthContext from '../../context/auth/authContext'
 import history from '../../utils/history'
 import Layout from '../../layout/user_layout'
+import InputEl from '../../components/input'
+
+import ipfs from '../../services/ipfs'
+import { IPFS_INFURA_URL } from '../../config'
+
+import { Button } from '../../components/button'
+
+import wallet_icon from '../../assets/icons/wallet.svg'
+import user_icon from '../../assets/icons/user.svg'
+import lock_icon from '../../assets/icons/lock.svg'
+import email_icon from '../../assets/icons/email.svg'
+import location_icon from '../../assets/icons/location.svg'
+import image_icon from '../../assets/icons/image.svg'
+import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
 	const [form] = Form.useForm()
@@ -15,15 +29,11 @@ const RegisterPage = () => {
 			wallet: accounts[0],
 		})
 	}, [accounts, form])
-
-	const onFinish = values => {
-		console.log('Success:', values)
-		register(values)
-	}
-
-	const onFinishFailed = errorInfo => {
-		console.log('Failed:', errorInfo)
-	}
+	window.ethereum.on('accountsChanged', accounts => {
+		form.setFieldsValue({
+			wallet: accounts[0],
+		})
+	})
 
 	useEffect(() => {
 		if (error) {
@@ -37,101 +47,191 @@ const RegisterPage = () => {
 		}
 	}, [success])
 
+	const [imageHash, setImageHash] = useState('')
+	const [uploading, setUploading] = useState(false)
+	const captureImage = e => {
+		setUploading(true)
+		const file = e.target.files[0]
+		const reader = new window.FileReader()
+		reader.readAsArrayBuffer(file)
+		reader.onloadend = () => {
+			const bufferArray = Buffer(reader.result)
+			console.log('buffer', bufferArray)
+			if (bufferArray) {
+				ipfs.add(bufferArray)
+					.then(result => {
+						console.log(result)
+						message.success('Uploaded to IPFS')
+						return setImageHash(result.path)
+					})
+					.catch(err => {
+						message.err('Error uploading to IPFS')
+						console.error(err)
+					})
+					.finally(() => setUploading(false))
+			}
+		}
+	}
+
+	const onFinish = values => {
+		const data = {
+			...values,
+			imageHash: imageHash,
+		}
+
+		register(data)
+	}
+
+	const onFinishFailed = errorInfo => {
+		// console.log('Failed:', errorInfo)
+	}
+
 	return (
 		<Layout>
-			<div style={{ height: '70vh' }} className='Center'>
-				<Form
-					// {...layout}
-					name='basic'
-					form={form}
-					initialValues={{}}
-					onFinish={onFinish}
-					onFinishFailed={onFinishFailed}
-				>
-					<Form.Item
-						label='Your Wallet'
-						name='wallet'
-						rules={[
-							{
-								required: true,
-								message: 'You need to have your wallet!',
-							},
-						]}
+			<div style={{ minHeight: '95vh' }} className='Center'>
+				<div>
+					<h2 style={{ color: '#294859' }}>
+						Register into Dfund Network
+					</h2>
+					<Form
+						name='basic'
+						form={form}
+						onFinish={onFinish}
+						onFinishFailed={onFinishFailed}
 					>
-						<Input disabled />
-					</Form.Item>
-					<Form.Item
-						label='Username'
-						name='username'
-						rules={[
-							{
-								required: true,
-								message: 'Please input your username!',
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						label='Email'
-						name='email'
-						rules={[
-							{
-								required: true,
-								message: 'Please input your email!',
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
+						<InputEl
+							label='Your Ethereum walllet'
+							name='wallet'
+							validationRule={[
+								{
+									required: true,
+									message: 'You need to have your wallet!',
+								},
+							]}
+							placeholder=''
+							icon={wallet_icon}
+							disabled={true}
+						/>
 
-					<Form.Item
-						label='Password'
-						name='password'
-						rules={[
-							{
-								required: true,
-								message: 'Please input your password!',
-							},
-						]}
-					>
-						<Input.Password />
-					</Form.Item>
-					<Form.Item
-						label='Country'
-						name='country'
-						rules={[
-							{
-								required: true,
-								message: 'Please input your country!',
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						label='Profile Picture'
-						name='imageHash'
-						rules={[
-							{
-								required: false,
-								message: 'Please input your country!',
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
+						<InputEl
+							label='Username'
+							name='username'
+							validationRule={[
+								{
+									required: true,
+									message: 'Please input your username!',
+								},
+							]}
+							placeholder='Username'
+							icon={user_icon}
+							disabled={false}
+						/>
 
-					<Form.Item>
-						<Button
-							type='primary'
-							htmlType='submit'
-							loading={loading}
-						>
-							Register
-						</Button>
-					</Form.Item>
-				</Form>
+						<InputEl
+							label='Email'
+							name='email'
+							validationRule={[
+								{
+									required: true,
+									message: 'Please input your email!',
+								},
+							]}
+							placeholder='Email Address'
+							icon={email_icon}
+							disabled={false}
+						/>
+
+						<InputEl
+							label='Country'
+							name='country'
+							validationRule={[
+								{
+									required: true,
+									message: 'Please input your country',
+								},
+							]}
+							placeholder='Country'
+							icon={location_icon}
+							disabled={false}
+						/>
+
+						<div>
+							<h4
+								style={{
+									marginBottom: '0.1rem',
+									opacity: 0.85,
+								}}
+							>
+								Profile Picture
+							</h4>
+							<Form.Item name='imageHash'>
+								<Input
+									prefix={
+										<img
+											src={image_icon}
+											alt=''
+											width='24'
+											height='24'
+											style={{ marginLeft: '-6px' }}
+										/>
+									}
+									type='file'
+									onChange={captureImage}
+									style={{
+										height: '2.5rem',
+										width: '26rem',
+										border: '1px solid rgba(0,0,0,0,0.2)',
+										borderLeft: '7px solid #5F66F1',
+									}}
+								/>
+							</Form.Item>
+							{uploading && <Spin />}
+							{imageHash !== '' && (
+								<img
+									src={`${IPFS_INFURA_URL}/${imageHash}`}
+									alt=''
+									height='100'
+									width='100'
+								/>
+							)}
+						</div>
+
+						<InputEl
+							label='Password'
+							name='password'
+							validationRule={[
+								{
+									required: true,
+									message: 'Please input your password!',
+								},
+							]}
+							placeholder='Password'
+							icon={lock_icon}
+							disabled={false}
+							isPasswordField={true}
+						/>
+
+						<Form.Item>
+							<Button
+								type='primary'
+								htmlType='submit'
+								variant='primary'
+								block
+								loading={loading}
+							>
+								Register
+							</Button>
+						</Form.Item>
+						<p>
+							Already have an Dfund account?{' '}
+							<Link to='/login'>
+								<strong style={{ textDecoration: 'underline' }}>
+									Login
+								</strong>
+							</Link>
+						</p>
+					</Form>
+				</div>
 			</div>
 		</Layout>
 	)
