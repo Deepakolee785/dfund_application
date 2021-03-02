79b116a376ed3f64c97855fa3e873e468090f1ab
Deepak Oli <deepakolee785@gmail.com>
2021-03-02 17:22:57 +0545

    create campaign style component

--- client/src/pages/create_campaign/index.js
+++ client/src/pages/create_campaign/index.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Form, Input, Button, DatePicker, InputNumber, Select } from 'antd'
-import TextArea from 'antd/lib/input/TextArea'
+import { Form, Select, Row, Col, Radio } from 'antd'
 import { useMutation } from 'react-query'
+import moment from 'moment'
 
 import FactoryContext from '../../context/factory/factoryContext'
 import { fromEtherToWei, createCampaign } from '../../api/web3Api'
@@ -11,12 +11,32 @@ import { saveCampaign } from '../../api/campaign'
 import Layout from '../../layout/user_layout'
 import Header from '../../components/header'
 import Uploader from '../../components/uploader'
-import { FormDiv } from './style'
+import {
+	DatePickerEl,
+	FormDiv,
+	InputEl,
+	InputNumberEl,
+	Label,
+	SelectEl,
+	SubText,
+	TextAreaEl,
+} from './style'
+import { Button } from '../../components/button'
+import LunchIcon from '../../assets/icons/startup.svg'
 const { Option } = Select
+
+const dateFormat = 'YYYY/MM/DD'
 
 const CreateCampaign = () => {
 	const history = useHistory()
+	const [campaignForm] = Form.useForm()
 	const { contract, accounts, web3 } = useContext(FactoryContext)
+	const [deadlineDaysValue, setDeadlineDaysValue] = useState(30)
+	useEffect(() => {
+		campaignForm.setFieldsValue({
+			deadline: moment(new Date(), dateFormat).add('days', 30),
+		})
+	}, [])
 	const create = useMutation(
 		data => createCampaign(contract, data, accounts[0]),
 		{
@@ -82,31 +102,6 @@ const CreateCampaign = () => {
 	)
 
 	const [imageHash, setImageHash] = useState('')
-	// const [uploading, setUploading] = useState(false)
-	// const captureImage = e => {
-	// 	setUploading(true)
-	// 	const file = e.target.files[0]
-	// 	const reader = new window.FileReader()
-	// 	reader.readAsArrayBuffer(file)
-	// 	reader.onloadend = () => {
-	// 		const bufferArray = Buffer(reader.result)
-	// 		console.log('buffer', bufferArray)
-	// 		if (bufferArray) {
-	// 			ipfs.add(bufferArray)
-	// 				.then(result => {
-	// 					console.log(result)
-	// 					message.success('Uploaded to IPFS')
-	// 					return setImageHash(result.path)
-	// 				})
-	// 				.catch(err => {
-	// 					message.err('Error uploading to IPFS')
-	// 					console.error(err)
-	// 				})
-	// 				.finally(() => setUploading(false))
-	// 		}
-	// 	}
-	// }
-
 	const onFinish = values => {
 		// console.log(values)
 		const data = {
@@ -145,7 +140,12 @@ const CreateCampaign = () => {
 			<Header heading='Create a new Dfund campaign here ' />
 			<br />
 			<FormDiv>
-				<Form name='createCampaign' onFinish={onFinish}>
+				<Form
+					name='createCampaign'
+					onFinish={onFinish}
+					form={campaignForm}
+				>
+					{/* Campaign Image Uploader */}
 					<Uploader
 						setImageHash={setImageHash}
 						label='Campaign Image'
@@ -153,150 +153,270 @@ const CreateCampaign = () => {
 							make it count and please provide a professional
 							photo.'
 					/>
-					<Form.Item
-						label='Title'
-						name='title'
-						rules={[
-							{
-								required: true,
-								message: 'Title is required',
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						label='Description'
-						name='description'
-						rules={[
-							{
-								required: true,
-								message: 'Description is required',
-							},
-						]}
-					>
-						<TextArea />
-					</Form.Item>
-					<Form.Item
-						name='category'
-						label='Category'
-						rules={[
-							{
-								required: true,
-								message: 'Cateogry is required',
-							},
-						]}
-					>
-						<Select placeholder='Select category' allowClear>
-							<Option value='food'>Food</Option>
-							<Option value='technology'>Technology</Option>
-							<Option value='art'>Art</Option>
-						</Select>
-					</Form.Item>
-					<Form.Item
-						name='country'
-						label='country'
-						rules={[
-							{
-								required: true,
-								message: 'Country is required',
-							},
-						]}
-					>
-						<Select
-							placeholder='Select Country'
-							// onChange={onGenderChange}
-							allowClear
-							showSearch
-							filterOption={(input, option) =>
-								option.children
-									.toLowerCase()
-									.indexOf(input.toLowerCase()) >= 0
-							}
+					<div>
+						<Label htmlFor='title'>Title of Campaign</Label>
+						<Form.Item
+							// label='Title'
+							name='title'
+							rules={[
+								{
+									required: true,
+									message: '*Campaign Title is required!',
+								},
+							]}
 						>
-							{countires.map((country, index) => {
-								return (
-									<Option key={index} value={country.name}>
-										{country.name}
-									</Option>
-								)
-							})}
-						</Select>
-					</Form.Item>
-					<Form.Item
-						label='Goal(ETH)'
-						name='goalAmount'
-						rules={[
-							{
-								required: true,
-								message: 'Goal amount is required',
-							},
-						]}
-					>
-						<InputNumber
-							style={{ width: '100%' }}
-							placeholder='Goal amount (in Eth)'
-						/>
-					</Form.Item>
-					<Form.Item
-						label='Min contribution(ETH)'
-						name='minContribution'
-						rules={[
-							{
-								required: true,
-								message: 'Min contribution is required',
-							},
-						]}
-					>
-						<InputNumber
-							style={{ width: '100%' }}
-							placeholder='min contribution (in Eth)'
-						/>
-					</Form.Item>
-					<Form.Item
-						label='Deadline'
-						name='deadline'
-						rules={[
-							{
-								required: true,
-								message: 'deadline is required',
-							},
-						]}
-					>
-						<DatePicker />
-					</Form.Item>
-					{/* <Form.Item
-						label='Image '
-						name='imagehash'
-						rules={[
-							{
-								required: true,
-								message: 'imagehash is required',
-							},
-						]}
-					>
-						<Input type='file' onChange={captureImage} />
-					</Form.Item>
-					{uploading && <Spin />}
-					{imageHash !== '' && (
-						<img
-							src={`${IPFS_INFURA_URL}/${imageHash}`}
-							alt=''
-							height='300'
-							width='300'
-						/>
-					)} */}
-					<br />
-					<Form.Item>
-						<Button
-							type='primary'
-							htmlType='submit'
-							loading={create.isLoading}
-							block
+							<InputEl placeholder='Eg. My new campaign for today' />
+						</Form.Item>
+						<SubText>
+							<span style={{ color: 'red', fontSize: '1rem' }}>
+								*
+							</span>
+							Your campaign title should be simple, specific and
+							memorable. Our search method run through these
+							sections of your campaign , so be sure to incoprate
+							any key words here! These will help people find your
+							campaign so choose them wisely.
+						</SubText>
+					</div>
+
+					<div>
+						<Label htmlFor='goalAmount'>
+							{`Goal amount (> 0.1 Eth)`}
+						</Label>
+						<Form.Item
+							name='goalAmount'
+							rules={[
+								{
+									required: true,
+									message: '*Goal amount is required!',
+								},
+							]}
 						>
-							Create new Dfund!
-						</Button>
-					</Form.Item>
+							<InputNumberEl
+								// style={{ width: '100%' }}
+								placeholder='Eg. 100 (in ETH)'
+							/>
+						</Form.Item>
+					</div>
+					<div>
+						<Label htmlFor='minContribution'>
+							{`Minimum Contribution (< goal amount)`}
+						</Label>
+						<Form.Item
+							name='minContribution'
+							rules={[
+								{
+									required: true,
+									message:
+										'*Minimum Contribution is required!',
+								},
+							]}
+						>
+							<InputNumberEl
+								// style={{ width: '100%' }}
+								placeholder='Eg. 10 (in ETH)'
+							/>
+						</Form.Item>
+						<SubText>
+							<span style={{ color: 'red', fontSize: '1rem' }}>
+								*
+							</span>
+							Minimum contribution is minimum amount a contributor
+							must fund in the project to be considered as approve
+							of campaign and can approve the any spending request
+							created by you.
+						</SubText>
+					</div>
+					<Row gutter={10}>
+						<Col flex='auto'>
+							<div>
+								<Label htmlFor='category'>
+									Select your category
+								</Label>
+								<Form.Item
+									name='category'
+									rules={[
+										{
+											required: true,
+											message: '*Cateogry is required!',
+										},
+									]}
+								>
+									<SelectEl
+										placeholder='Select category'
+										allowClear
+									>
+										<Option value='food'>Food</Option>
+										<Option value='technology'>
+											Technology
+										</Option>
+										<Option value='art'>Art</Option>
+									</SelectEl>
+								</Form.Item>
+							</div>
+						</Col>
+						<Col flex='auto'>
+							<div>
+								<Label htmlFor='country'>
+									Select your Country
+								</Label>
+								<Form.Item
+									name='country'
+									rules={[
+										{
+											required: true,
+											message: '*Country is required!',
+										},
+									]}
+								>
+									<SelectEl
+										placeholder='Select Country'
+										allowClear
+										showSearch
+										filterOption={(input, option) =>
+											option.children
+												.toLowerCase()
+												.indexOf(input.toLowerCase()) >=
+											0
+										}
+									>
+										{countires.map((country, index) => {
+											return (
+												<Option
+													key={index}
+													value={country.name}
+												>
+													{country.name}
+												</Option>
+											)
+										})}
+									</SelectEl>
+								</Form.Item>
+							</div>
+						</Col>
+					</Row>
+
+					<div>
+						<Label htmlFor='description'>
+							Enter your campaign story
+						</Label>
+						<Form.Item
+							name='description'
+							rules={[
+								{
+									required: true,
+									message: '*Campaign Story is required!',
+								},
+							]}
+						>
+							<TextAreaEl placeholder='Your story goes here...' />
+						</Form.Item>
+					</div>
+
+					<div>
+						<Label htmlFor='deadline'>
+							Duration of your campaign
+						</Label>
+						<Row gutter={[10, 10]} justify='space-between'>
+							<Col>
+								<Radio.Group
+									value={deadlineDaysValue}
+									onChange={e => {
+										// console.log(e.target.value)
+										const days = e.target.value
+										setDeadlineDaysValue(days)
+										// console.log(new Date())
+										campaignForm.setFieldsValue({
+											deadline: moment(
+												new Date(),
+												dateFormat
+											).add('days', days),
+										})
+									}}
+								>
+									<Radio.Button
+										value={15}
+										style={{
+											width: '12rem',
+											textAlign: 'center',
+										}}
+									>
+										15 Days
+									</Radio.Button>
+									<Radio.Button
+										value={30}
+										style={{
+											width: '12rem',
+											textAlign: 'center',
+										}}
+									>
+										30 Days
+									</Radio.Button>
+									<Radio.Button
+										value={60}
+										style={{
+											width: '12rem',
+											textAlign: 'center',
+										}}
+									>
+										60 days
+									</Radio.Button>
+								</Radio.Group>
+							</Col>
+
+							<Col style={{ marginTop: '-0.6rem' }}>
+								<hr />
+								OR
+								<hr />
+							</Col>
+							<Col flex='auto'>
+								<Form.Item
+									name='deadline'
+									rules={[
+										{
+											required: true,
+											message: '*Deadline is required!',
+										},
+									]}
+								>
+									<DatePickerEl
+										format={dateFormat}
+										style={{ width: '100%' }}
+									/>
+								</Form.Item>
+							</Col>
+						</Row>
+					</div>
+
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+						}}
+					>
+						<Form.Item>
+							<Button
+								type='primary'
+								htmlType='submit'
+								loading={create.isLoading}
+								// block
+								variant='primary'
+								icon={
+									<img
+										src={LunchIcon}
+										alt=''
+										style={{
+											width: '20px',
+											marginRight: '0.3rem',
+										}}
+									/>
+								}
+							>
+								Lunch your new Dfund!
+							</Button>
+						</Form.Item>
+					</div>
 				</Form>
 			</FormDiv>
 		</Layout>
--- client/src/pages/create_campaign/style.js
+++ client/src/pages/create_campaign/style.js
@@ -1,6 +1,102 @@
+import { DatePicker, Input, InputNumber, Select } from 'antd'
+import TextArea from 'antd/lib/input/TextArea'
 import styled from 'styled-components'
 
 export const FormDiv = styled.div`
 	width: 80%;
 	margin: 0 auto;
+`
+
+export const Label = styled.label`
+	font-style: normal;
+	font-weight: 600;
+	font-size: 22px;
+	line-height: 33px;
+	/* identical to box height */
+
+	text-transform: uppercase;
+
+	color: rgba(0, 0, 0, 0.71);
+`
+export const InputEl = styled(Input)`
+	padding: 0;
+	&.ant-input {
+		border: 0;
+		border-bottom: 2px solid rgba(0, 0, 0, 0.24);
+	}
+	&.ant-input:hover,
+	&.ant-input:focus,
+	&.ant-input:active {
+		border: 0;
+		outline: none;
+		box-shadow: none;
+		border-bottom: 2px solid #5f66f1;
+	}
+`
+export const InputNumberEl = styled(InputNumber)`
+	width: 100%;
+	.ant-input-number-input {
+		padding: 0;
+	}
+	&.ant-input-number {
+		border: 0;
+		outline: none !important;
+		border-bottom: 2px solid rgba(0, 0, 0, 0.24);
+	}
+	&.ant-input-number:hover,
+	&.ant-input-number-focused,
+	&.ant-input-number:active {
+		border: 0;
+		outline: none;
+		box-shadow: none;
+		border-bottom: 2px solid #5f66f1;
+	}
+`
+export const SelectEl = styled(Select)`
+	&.ant-select {
+		border: 0;
+		outline: none !important;
+		/* border-bottom: 2px solid rgba(0, 0, 0, 0.24); */
+	}
+`
+export const TextAreaEl = styled(TextArea)`
+	padding: 0;
+	&.ant-input {
+		border: 0;
+		border-bottom: 2px solid rgba(0, 0, 0, 0.24);
+	}
+	&.ant-input:hover,
+	&.ant-input:focus,
+	&.ant-input:active {
+		border: 0;
+		outline: none;
+		box-shadow: none;
+		border-bottom: 2px solid #5f66f1;
+	}
+`
+
+export const DatePickerEl = styled(DatePicker)`
+	padding: 0;
+	&.ant-picker {
+		border: 0;
+		border-bottom: 2px solid rgba(0, 0, 0, 0.24);
+	}
+	&.ant-picker:hover,
+	&.ant-picker:focus,
+	&.ant-picker:active {
+		border: 0;
+		outline: none;
+		box-shadow: none;
+		border-bottom: 2px solid #5f66f1;
+	}
+`
+
+export const SubText = styled.p`
+	font-style: normal;
+	font-weight: 400;
+	font-size: 12px;
+	line-height: 14px;
+	letter-spacing: 0.02em;
+	/* margin-top: -1.2rem; */
+	color: rgba(0, 0, 0, 0.32);
 `
