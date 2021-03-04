3c38d050b7a2aa715aab96ff2ca22fc0d1270548
Deepak Oli <deepakolee785@gmail.com>
2021-03-04 08:48:45 +0545

    container component

---
+++ client/src/components/container/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+export const Container = styled.div`
+	width: 80%;
+	margin: 0 auto;
+	/* display: flex;
+	align-items: center;
+	justify-content: space-between;
+	justify-items: center;
+	height: 4.5rem; */
+`
