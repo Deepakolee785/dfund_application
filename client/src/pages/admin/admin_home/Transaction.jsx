f5136a0748e74c5bb40baa4cd393e2b04e430418
Deepak Oli <deepakolee785@gmail.com>
2021-03-05 13:13:08 +0545

    all transaction fetched and shown in table

--- client/src/pages/admin/admin_home/Transaction.jsx
+++ client/src/pages/admin/admin_home/Transaction.jsx
@@ -1,20 +1,119 @@
 import { useEffect, useState } from 'react'
 import { getAllTransactions } from '../../../api/transaction'
-
+import { v4 as uuid } from 'uuid'
+import { Table } from 'antd'
 const Transaction = () => {
-	const [state, setstate] = useState(null)
+	const [transaction, setTransactions] = useState(null)
 	useEffect(() => {
 		getAllTransactions()
 			.then(res => {
 				// console.log(res)
-				setstate(res.data.transactions)
+				setTransactions(res.data.transactions)
 			})
 			.catch(err => console.log(err))
 	}, [])
+
+	const columns = [
+		{
+			title: 'Sender',
+			dataIndex: 'sender',
+			key: 'sender',
+		},
+		{
+			title: 'Reciver',
+			dataIndex: 'reciver',
+			key: 'reciver',
+		},
+		{
+			title: 'Campaign',
+			dataIndex: 'projectAddress',
+			key: 'projectAddress',
+		},
+		{
+			title: 'Transaction Type',
+			dataIndex: 'transactionType',
+			key: 'transactionType',
+		},
+		{
+			title: 'Amount',
+			dataIndex: 'amount',
+			key: 'amount',
+		},
+		{
+			title: 'Date',
+			dataIndex: 'createdAt',
+			key: 'createdAt',
+		},
+		{
+			title: 'Block Hash',
+			dataIndex: 'blockHash',
+			key: 'blockHash',
+		},
+		{
+			title: 'Block Number',
+			dataIndex: 'blockNumber',
+			key: 'blockNumber',
+		},
+		{
+			title: 'Gas Used',
+			dataIndex: 'cumulativeGasUsed',
+			key: 'cumulativeGasUsed',
+		},
+		{
+			title: 'status',
+			dataIndex: 'status',
+			key: 'status',
+		},
+		{
+			title: 'Transaction Hash',
+			dataIndex: 'transactionHash',
+			key: 'transactionHash',
+		},
+	]
+
+	const dataSource =
+		transaction &&
+		transaction.map(transaction => {
+			const {
+				user,
+				sender,
+				amount,
+				reciver,
+				blockHash,
+				blockNumber,
+				cumulativeGasUsed,
+				status,
+				transactionHash,
+				projectAddress,
+				transactionType,
+				createdAt,
+			} = transaction
+			return {
+				key: uuid(),
+				user,
+				sender,
+				amount,
+				reciver,
+				blockHash,
+				blockNumber,
+				cumulativeGasUsed,
+				status,
+				transactionHash,
+				projectAddress,
+				transactionType,
+				createdAt,
+			}
+		})
 	return (
 		<div>
-			Transaction
-			<pre>{state && JSON.stringify(state, null, 6)}</pre>
+			<h1>Transaction</h1>
+			<Table
+				dataSource={dataSource}
+				columns={columns}
+				scroll={{ x: true }}
+			/>
+			;
+			{/* <pre>{transaction && JSON.stringify(transaction, null, 6)}</pre> */}
 		</div>
 	)
 }
