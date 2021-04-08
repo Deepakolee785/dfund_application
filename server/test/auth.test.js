9024fe541ead351fcdaec470fd07e78a48eafee7
Deepak Oli <deepakolee785@gmail.com>
2021-04-08 15:28:33 +0545

    auth user more unit tests

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -38,6 +38,35 @@ describe('User', () => {
 					done()
 				})
 		})
+		it('Should not register user if user already exists', done => {
+			chai
+				.request(server)
+				.post('/api/user/register')
+				.send(user)
+				.end((err, res) => {
+					res.should.have.status(200)
+					res.body.should.be.a('object')
+					res.body.should.have.property('user')
+					res.body.should.have
+						.property('message')
+						.eql('Successfully registered')
+
+					chai
+						.request(server)
+						.post('/api/user/register')
+						.send(user)
+						.end((err, res) => {
+							res.should.have.status(400)
+							// console.log(res.body)
+							res.body.should.be.a('object')
+							res.body.should.have
+								.property('message')
+								.eql('Wallet is already used.')
+
+							done()
+						})
+				})
+		})
 	})
 
 	/**
@@ -73,6 +102,36 @@ describe('User', () => {
 							res.should.have.status(200)
 							res.body.should.be.a('object')
 							res.body.should.have.property('token')
+							done()
+						})
+				})
+		})
+		it('Should not be able to login with invalid credentials', done => {
+			//register
+			chai
+				.request(server)
+				.post('/api/user/register')
+				.send(user)
+				.end((err, res) => {
+					res.should.have.status(200)
+					res.body.should.be.a('object')
+					res.body.should.have.property('user')
+					res.body.should.have
+						.property('message')
+						.eql('Successfully registered')
+
+					// login
+					chai
+						.request(server)
+						.post('/api/user/login')
+						.send({ ...loginUser, password: 'kjhkj' })
+						.end((err, res) => {
+							// console.log('res', res.body)
+							res.should.have.status(400)
+							res.body.should.be.a('object')
+							res.body.should.have
+								.property('message')
+								.eql('Password is incorrect.')
 							done()
 						})
 				})
@@ -114,6 +173,47 @@ describe('User', () => {
 									res.should.have.status(200)
 									res.body.should.be.a('object')
 									res.body.should.have.property('username').eql('test_username')
+									done()
+								})
+						})
+				})
+		})
+		it('Should not get auth user using user invalid jwt token', done => {
+			chai
+				.request(server)
+				.post('/api/user/register')
+				.send(user)
+				.end((err, res) => {
+					res.should.have.status(200)
+					res.body.should.be.a('object')
+					res.body.should.have.property('user')
+					res.body.should.have
+						.property('message')
+						.eql('Successfully registered')
+
+					// login
+					chai
+						.request(server)
+						.post('/api/user/login')
+						.send(loginUser)
+						.end((err, res) => {
+							// console.log('res', res.body)
+							res.should.have.status(200)
+							res.body.should.be.a('object')
+							res.body.should.have.property('token')
+
+							let token = res.body.token
+
+							//
+							chai
+								.request(server)
+								.get('/api/user/authUser')
+								.end((err, res) => {
+									res.should.have.status(401)
+									res.body.should.be.a('object')
+									res.body.should.have
+										.property('msg')
+										.eql('No token, authorization denied')
 									done()
 								})
 						})
